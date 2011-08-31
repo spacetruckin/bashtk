1c4d3f20eb8166f7bec70da5f2d15ecb8efefad6
Rajiv Narayan <askrajiv@gmail.com>
2011-08-30 23:10:42 -0400

    code cleanup name changes

--- demo.sh
+++ demo.sh
@@ -1,54 +1,59 @@
 #!/bin/bash
-source ./bashtk
+source ./bashtk -q
+str_rep '-' 11
+msg 'BashTk demo' red bold
+str_rep '-' 11
 
 #start a timer
 tic
 #start logging
-logon bashtk.log
-logreset
+log_on bashtk.log
+log_reset
 
-msg 'Message functions' red underline
+msg 'Message functions' cyan underline
 msg 'msg supports color and logging' green
-msg 'This is in light-blue underlined' blue bold underline
-color green
-echo 'Im green'
+msg 'This line is in light-magenta and underlined.' magenta bold underline
+color cyan
+echo 'In cyan'
 color reset
-echo 'Back to default color'
-echo 'Highlight terms in a string' | colorize 'terms'
+echo 'Reset to default color.'
 echo
 
-msg 'Init style status messages' red underline
-stat_busy Running
+msg 'Init-style status notification' cyan underline
+stat_busy 'Running...'
 sleep 2
+stat_append 'done'
 stat_pass
-
 status 'Status of ls -l' ls -l 
 stat_busy 'Test 0 > 1'
 [[ 0 -gt 1 ]] && stat_pass|| stat_fail
 echo
 
-msg 'Math functions' red underline
-x=(`rand 5`)
+msg 'Math functions' cyan underline
+x={1..10}
 echo "x=${x[@]}"
 msg 'length x='`length ${x[@]}`
 msg 'sum x ='`sum ${x[@]}`
 msg 'mean x ='`mean ${x[@]}`
 msg 'Commands accept pipes: echo {1..5}|pow2|sqrt'
 echo {1..5} | pow2 |sqrt
-msg "Evaluate expressions: echo 'sqrt(2)*100/2' | calc"
-echo 'sqrt(2)*100/2' | calc
+msg "Evaluate expressions: calc 'sqrt(2)*100/2'"
+calc 'sqrt(2)*100/2'
 msg
 
-msg 'String utilities' red underline
+msg 'String utilities' cyan underline
 echo 'upperCase'|upper
 echo 'LOwercase'|lower
 echo 'capiTalize'|capitalize
 echo '  >strtrim<  '|str_trim
+echo 'Highlight terms in a string' | colorize 'terms' 'a'
+str_rep '-x' 25
+log_off
+echo
+
+msg "Playback log:" cyan underline
+log_cat
 str_rep '-' 20
 
-echo -e "\nPlayback log:"
-logcat
-logoff
 #time elapsed
-str_rep '-' 20
 echo "Time elapsed $(toc)(s)."
